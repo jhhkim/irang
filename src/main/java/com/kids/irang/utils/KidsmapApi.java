aeccd6a3ed371102a661b4f279b18bbfb012720e
jhhkim <aekim7@naver.com>
2022-01-06 14:08:00 +0900

    alt 태그 안 내용 스크래핑하게

--- src/main/java/com/kids/irang/utils/KidsmapApi.java
+++ src/main/java/com/kids/irang/utils/KidsmapApi.java
@@ -1,51 +1,42 @@
-package com.kids.irang.utils;
+// package com.kids.irang.utils;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+// import java.io.IOException;
 
+// import com.kids.irang.dto.KidsmapDto;
 
+// import org.jsoup.Jsoup;
+// import org.jsoup.nodes.Document;
+// import org.jsoup.nodes.Element;
+// import org.jsoup.select.Elements;
 
-import com.kids.irang.dto.KidsmapDto;
+// public class KidsmapApi {
+//         try{
+//             String URL = "https://www.mangoplate.com/search/%EA%B8%B0%EC%9E%A5%EA%B5%B0";
+//             Document doc = Jsoup.connect(URL).get();
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+//             Elements store = doc.select("div>a>h2.title");
+//             Elements addr = doc.select("div.thumb>img.center-croping lazy");
+//             addr.attr("alt"); //"사진 - "을 기준으로 자르고 addr[1]을 db에 넣어야하는데
+//             Elements category = doc.select("div>p.etc>span");
 
-public class KidsmapApi {
-    public static List<KidsmapDto> getKidsmapList(){
-        List<KidsmapDto> list = new ArrayList<KidsmapDto>();
-        String URL = "https://www.mangoplate.com/search/%EA%B8%B0%EC%9E%A5%EA%B5%B0";
+//             for(Element e: store)
+//             {
+                
+//             }
+//             for(Element e : addr)
+//             {
+                
+//             } 
+//             for(Element e : category)
+//             {
+                
+//             } 
 
-        try{
-            Document doc = Jsoup.connect(URL).get();
-
-            Elements store = doc.select("div>a>h2.title");
-            Elements addr = doc.select("div.thumb>img.center-croping lazy");
-            addr.attr("alt"); //"사진 - "을 기준으로 자르고 addr[1]을 db에 넣어야하는데
-            Elements category = doc.select("div>p.etc>span");
-
-
-            List<String> tmpList = new ArrayList<>();
-            for(Element e: store)
-            {
-                tmpList.add(e.text());
-            }
-            for(Element e : addr)
-            {
-                tmpList.add(e.text());
-            } 
-            for(Element e : category)
-            {
-                tmpList.add(e.text());
-            } 
-
-        }
-        catch (IOException e1){
-            e1.printStackTrace();
-        }
-        return list;
+//         }
+//         catch (IOException e1){
+//             e1.printStackTrace();
+//         }
+//         return;
            
-    }
-}
+//     }
+// }
--- src/main/java/com/kids/irang/web/IndexController.java
+++ src/main/java/com/kids/irang/web/IndexController.java
@@ -1,5 +1,12 @@
 package com.kids.irang.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
-    
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
